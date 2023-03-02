2ecf8bfbb503c0fced12ac7df7d5786977ff8104
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-02 21:55:57 +0100

    Create Keyboard component

---
+++ src/components/Keyboard/Keyboard.tsx
@@ -0,0 +1,26 @@
+import EqualButton from "../EqualButton/EqualButton";
+import Key from "../Button/Button";
+import "./keyBoardStyled.css";
+const Keyboard = (): JSX.Element => {
+  return (
+    <ul className="calculator__keyboard">
+      <Key keyValue={0} />
+      <Key keyValue={1} />
+      <Key keyValue={2} />
+      <Key keyValue={3} />
+      <Key keyValue={4} />
+      <Key keyValue={5} />
+      <Key keyValue={6} />
+      <Key keyValue={7} />
+      <Key keyValue={8} />
+      <Key keyValue={9} />
+      <Key keyValue={"/"} />
+      <Key keyValue={"*"} />
+      <Key keyValue={"+"} />
+      <Key keyValue={"-"} />
+      <EqualButton />
+    </ul>
+  );
+};
+
+export default Keyboard;
